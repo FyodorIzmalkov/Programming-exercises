f46bc8c3bae0b1fdc94a68e29dc1b1d1686c9c16
Teorell <fedorizmalkov@gmail.com>
2019-06-28 13:28:12 +0300

    1

---
+++ Easy/Difficulty700/Cf344A.java
@@ -0,0 +1,21 @@
+package Difficulty700;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cf344A {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 1;
+        int currentNum;
+        int previousNum = 0;
+        while(n > 0){
+            currentNum = Integer.parseInt(br.readLine());
+            previousNum = currentNum;
+            --n;
+        }
+    }
+}

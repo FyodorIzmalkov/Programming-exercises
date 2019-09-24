a8d5d7ad12bdacb9bc8336e4191dc395f1f682a6
Teorell <fedorizmalkov@gmail.com>
2019-09-24 09:06:37 +0300

    1

--- Easy/LeetCodeEz/removeOuterParentheses.java
+++ Easy/LeetCodeEz/removeOuterParentheses.java
@@ -1,23 +1,31 @@
 package LeetCodeEz;
 
+import java.util.Stack;
+
 public class removeOuterParentheses {
     public String removeOuterParentheses(String S) {
         StringBuilder result = new StringBuilder("");
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < S.length(); i++) {
             char ch = S.charAt(i);
-            if (ch =='('){
-                int num = 0;
-                i++;
-                while (num >= 0 && i < S.length()) {
-                    if (S.charAt(i) == '(') {
-                        result.append(S.charAt(i));
-                        num++;
-                        i++;
-                    } else if (S.charAt(i) == ')' && num > 0) {
-                        result.append(S.charAt(i));
-                        num--;
-                        i++;
-                    }
+            if (ch == '('){
+                if (stack.empty()){
+                    stack.push(ch);
+                    continue;
+                }
+                else {
+                    stack.push(ch);
+                    result.append(ch);
+                }
+            }
+            else if (ch == ')'){
+                if (stack.size() == 1){
+                    stack.pop();
+                    continue;
+                }
+                else{
+                    stack.pop();
+                    result.append(ch);
                 }
             }
         }

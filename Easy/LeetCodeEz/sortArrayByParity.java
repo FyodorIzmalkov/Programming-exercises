8441d7487d25844faf605610d1edc17f4290888d
Teorell <fedorizmalkov@gmail.com>
2019-09-26 22:37:29 +0300

    1

---
+++ Easy/LeetCodeEz/sortArrayByParity.java
@@ -0,0 +1,26 @@
+package LeetCodeEz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class sortArrayByParity {
+    public int[] sortArrayByParity(int[] A) {
+        List<Integer> even = new ArrayList<>(A.length);
+        List<Integer> odd = new ArrayList<>(A.length);
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0)
+                even.add(A[i]);
+            else
+                odd.add(A[i]);
+        }
+        int i = 0;
+        for (i = 0; i < even.size(); i++) {
+            A[i] = even.get(i);
+        }
+        for (int j = 0; j < odd.size(); j++) {
+            A[i] = odd.get(j);
+            i++;
+        }
+        return  A;
+    }
+}

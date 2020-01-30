2d1656b944c690bfb0883ce9ba2e42a6d14478dd
Teorell <fedorizmalkov@gmail.com>
2020-01-31 00:06:08 +0300

    some java problems solved

---
+++ Easy/hackerrank/java/StringTask1.java
@@ -0,0 +1,17 @@
+package hackerrank.java;
+
+import java.util.Scanner;
+
+public class StringTask1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        int a_len = A.length();
+        int b_len = B.length();
+
+        System.out.println(a_len + b_len);
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
+    }
+}
---
+++ Easy/hackerrank/java/StringTask6.java
@@ -0,0 +1,22 @@
+package hackerrank.java;
+
+import java.util.Scanner;
+
+public class StringTask6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+        s = s.trim();
+        if (s.isEmpty()){
+            System.out.println(0);
+            return;
+        }
+        String [] result = s.split("[ !,?._'@]+");
+        System.out.println(result.length);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+}

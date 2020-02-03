65bd3c39e5ea39cbd361f2a4258504e46e52ab0f
Teorell <fedorizmalkov@gmail.com>
2020-02-04 00:20:08 +0300

    big decimal

---
+++ Easy/hackerrank/java/BigDecimal1.java
@@ -0,0 +1,14 @@
+package hackerrank.java;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.math.BigDecimal;
+
+public class BigDecimal1 {
+    public static void main(String[] args) {
+        BigDecimal bigDecimal = new BigDecimal("0.01");
+        BigDecimal bigDecimal2 = new BigDecimal(".01");
+        System.out.println(bigDecimal);
+        System.out.println(bigDecimal2);
+    }
+}

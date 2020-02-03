dd94ddba4fe976949113466868c5ddfe44b8a7c5
Teorell <fedorizmalkov@gmail.com>
2020-02-03 23:49:57 +0300

    some regexp

--- Easy/hackerrank/java/ParserTask1.java
+++ Easy/hackerrank/java/ParserTask1.java
@@ -8,18 +8,22 @@ import java.math.*;
 import java.util.regex.*;
 
 public class ParserTask1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         int testCases = Integer.parseInt(in.nextLine());
-        String matcher = "/(<){1}(\\w+>)(\\w+)(<){1}(/\\2)/";
+        String matcher = "<(.+)>([^<]+)</\\1>";
         Pattern pattern = Pattern.compile(matcher);
-        while(testCases>0){
+        while (testCases > 0) {
             String line = in.nextLine();
             Matcher m = pattern.matcher(line);
-            while(m.find()){
-                System.out.println(m.group(0));
-                System.out.println(m.group(3));
+            boolean matchFound = false;
+            while (m.find()) {
+                matchFound = true;
+                System.out.println(m.group(2));
+            }
+            if (!matchFound){
+                System.out.println("None");
             }
             testCases--;
         }

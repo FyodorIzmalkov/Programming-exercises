6fe39a373a9c495ae2e7adf8add1c44e17d27008
Teorell <fedorizmalkov@gmail.com>
2020-02-04 23:54:23 +0300

    some java tasks

--- Easy/hackerrank/java/BigDecimal1.java
+++ Easy/hackerrank/java/BigDecimal1.java
@@ -1,14 +1,27 @@
 package hackerrank.java;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BigDecimal1 {
+
     public static void main(String[] args) {
-        BigDecimal bigDecimal = new BigDecimal("0.01");
-        BigDecimal bigDecimal2 = new BigDecimal(".01");
-        System.out.println(bigDecimal);
-        System.out.println(bigDecimal2);
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+
+        Comparator<String> comparator = (o1, o2) -> new BigDecimal(o2).compareTo(new BigDecimal(o1));
+        s = Arrays.stream(s).filter(Objects::nonNull).sorted(comparator).toArray(String[]::new);
+
+        //Output
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
     }
 }

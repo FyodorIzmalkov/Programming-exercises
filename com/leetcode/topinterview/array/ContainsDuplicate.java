58f54f5676934760dd41534479f403563674edd8
Teorell <fedorizmalkov@gmail.com>
2021-10-18 23:59:46 +0300

    +2 solution contains duplicate

--- com/leetcode/topinterview/array/ContainsDuplicate.java
+++ com/leetcode/topinterview/array/ContainsDuplicate.java
@@ -1,6 +1,9 @@
 package leetcode.topinterview.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
@@ -23,8 +26,27 @@ public class ContainsDuplicate {
         System.out.println(containsDuplicate(nums));
     }
 
-    //1
+    // 3 8.28%
+    // Runtime: 18 ms
+    //Memory Usage: 53.2 MB
     public static boolean containsDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count() != nums.length;
+    }
+
+    // 2 35.43%
+    public static boolean containsDuplicate2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>(nums.length);
+        for (int curVal : nums) {
+            if (set.contains(curVal)) {
+                return true;
+            }
+            set.add(curVal);
+        }
+        return false;
+    }
+
+    //1 13%
+    public static boolean containsDuplicate1(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>(nums.length);
         for (int curVal : nums) {
             if (map.get(curVal) != null) {

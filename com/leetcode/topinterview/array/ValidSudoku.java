76f8fa8ebf4abfea5e36abc141e873e730efa006
Teorell <fedorizmalkov@gmail.com>
2021-10-27 00:47:29 +0300

    sudoku solved

--- com/leetcode/topinterview/array/ValidSudoku.java
+++ com/leetcode/topinterview/array/ValidSudoku.java
@@ -1,5 +1,8 @@
 package leetcode.topinterview.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
  * <p>
@@ -10,6 +13,16 @@ package leetcode.topinterview.array;
  * <p>
  * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
  * Only the filled cells need to be validated according to the mentioned rules.
+ *
+ * [['.','.','.','.','5','.','.','1','.'],
+ * ['.','4','.','3','.','.','.','.','.'],
+ * ['.','.','.','.','.','3','.','.','1'],
+ * ['8','.','.','.','.','.','.','2','.'],
+ * ['.','.','2','.','7','.','.','.','.'],
+ * ['.','1','5','.','.','.','.','.','.'],
+ * ['.','.','.','.','.','2','.','.','.'],
+ * ['.','2','.','9','.','.','.','.','.'],
+ * ['.','.','4','.','.','.','.','.','.']]
  */
 public class ValidSudoku {
     public static void main(String[] args) {
@@ -23,14 +36,91 @@ public class ValidSudoku {
                 , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
                 , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
                 , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
-        System.out.println(isValidSudoku(sudoku));
+        
+        char[][] sudoku2 =   {{'.','.','.','.','5','.','.','1','.'},
+  {'.','4','.','3','.','.','.','.','.'},
+  {'.','.','.','.','.','3','.','.','1'},
+  {'8','.','.','.','.','.','.','2','.'},
+  {'.','.','2','.','7','.','.','.','.'},
+  {'.','1','5','.','.','.','.','.','.'},
+  {'.','.','.','.','.','2','.','.','.'},
+  {'.','2','.','9','.','.','.','.','.'},
+  {'.','.','4','.','.','.','.','.','.'}};
+        System.out.println(isValidSudoku(sudoku2));
 
     }
 
+    //Runtime: 4 ms
+    //Memory Usage: 39.2 MB
     public static boolean isValidSudoku(char[][] board) {
-        int [][] intBoard = new int[9][9];
+        int[][] intBoard = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char curChar = board[i][j];
+                if (curChar == '.') {
+                    intBoard[i][j] = 0;
+                } else {
+                    intBoard[i][j] = Integer.parseInt(String.valueOf(curChar));
+                }
+            }
+        }
+        Map<Integer, Integer> rowDuplicates = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                Integer curVal = intBoard[i][j];
+                if (!rowDuplicates.containsKey(curVal)) {
+                    rowDuplicates.put(curVal, 1);
+                } else {
+                    if (curVal > 0) {
+                        return false;
+                    }
+                }
+            }
+            rowDuplicates.clear();
+        }
 
+        Map<Integer, Integer> columnDuplicates = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                Integer curVal = intBoard[j][i];
+                if (!columnDuplicates.containsKey(curVal)) {
+                    columnDuplicates.put(curVal, 1);
+                } else {
+                    if (curVal > 0) {
+                        return false;
+                    }
+                }
+            }
+            columnDuplicates.clear();
+        }
 
-        return true;
+        return checkSquare(0, 3, 0, 3, intBoard);
+    }
+
+    public static boolean checkSquare(int fromRow, int toRow, int fromColumn, int toColumn, int[][] intBoard) {
+        Map<Integer, Integer> squareDuplicates = new HashMap<>();
+        if (toColumn > 9 && toRow < 9) {
+            fromRow += 3;
+            toRow += 3;
+            fromColumn = 0;
+            toColumn = 3;
+        } else if (toColumn > 9 && toRow == 9){
+            return true;
+        }
+        int fromColumnTmp = fromColumn;
+        for (; fromRow < toRow; fromRow++) {
+            fromColumn = fromColumnTmp;
+            for (; fromColumn < toColumn; fromColumn++) {
+                Integer curVal = intBoard[fromRow][fromColumn];
+                if (!squareDuplicates.containsKey(curVal)) {
+                    squareDuplicates.put(curVal, 1);
+                } else {
+                    if (curVal > 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return checkSquare(fromRow - 3, toRow, fromColumn, toColumn + 3, intBoard);
     }
 }
